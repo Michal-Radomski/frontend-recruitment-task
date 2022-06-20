3cd94eacc2002324b8ec10a9eac5dec7a763c0ec
Michal Radomski <michalra@op.pl>
2022-06-20 03:35:28 +0200

    Migration to arrow functions, close modal on esc key done

--- src/js/main.js
+++ src/js/main.js
@@ -4,20 +4,31 @@ const modal = document.querySelector(".modal");
 const closeModal = document.querySelector(".close-modal");
 
 // Open modal
-openModal.onclick = function () {
+openModal.onclick = () => {
   modal.style.display = "block";
   // console.log("Modal was opened");
 };
 
 // Close modal by X button
-closeModal.onclick = function () {
+closeModal.onclick = () => {
   modal.style.display = "none";
   // console.log("Modal was closed");
 };
 
 // Close modal when a user clicks outside of the modal
-window.onclick = function (event) {
+window.onclick = (event) => {
+  // console.log({event});
   if (event.target === modal) {
     modal.style.display = "none";
+    // console.log("Modal was closed");
   }
 };
+
+// Close modal by pressing Escape
+document.addEventListener("keydown", function (event) {
+  // console.log({event});
+  if (event.key === "Escape") {
+    modal.style.display = "none";
+    // console.log("Modal was closed");
+  }
+});
