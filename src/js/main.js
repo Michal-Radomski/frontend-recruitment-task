b0b1a580273fe8b2f97e840a17d260912aadba59
Michal Radomski <michalra@op.pl>
2022-06-20 04:55:54 +0200

    main.js done

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,28 @@
-//DOM
+// DOM
 const openModal = document.querySelector("#open-modal");
 const modal = document.querySelector(".modal");
 const closeModal = document.querySelector(".close-modal");
+const resetButton = document.querySelector(".button-reset");
+const spanCounter = document.querySelector("#spanCounter");
+
+// Initial counter value
+const counterValueFromLocalStorage = localStorage.getItem("Counter");
+// console.log({counterValueFromLocalStorage});
+
+let counter = counterValueFromLocalStorage ? Number(counterValueFromLocalStorage) : 0;
+// console.log({counter});
+spanCounter.innerText = `${counter} times`;
+resetButton.style.visibility = "hidden";
 
 // Open modal
 openModal.onclick = () => {
   modal.style.display = "block";
   // console.log("Modal was opened");
+  counter += 1;
+  // console.log({counter});
+  spanCounter.innerText = `${counter} times`;
+  counter >= 6 ? (resetButton.style.visibility = "visible") : (resetButton.style.visibility = "hidden");
+  localStorage.setItem("Counter", counter);
 };
 
 // Close modal by X button
@@ -32,3 +48,16 @@ document.addEventListener("keydown", function (event) {
     // console.log("Modal was closed");
   }
 });
+
+// Reset Counter
+const resetCounter = () => {
+  counter = 0;
+  // console.log({counter});
+  spanCounter.innerText = `${counter} times`;
+  resetButton.style.visibility = "hidden";
+  localStorage.setItem("Counter", counter);
+  setTimeout(function () {
+    modal.style.display = "none";
+  }, 500);
+};
+resetButton.addEventListener("click", resetCounter);
